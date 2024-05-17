28f7ad71d029796f4485976ee552d44fb04eac0f
Lsccccc <142605575+Lsccccc@users.noreply.github.com>
2024-05-17 21:27:14 +0800

    Fixed Bugs

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ pieces = {
 def print_board():
     for i in range(5, -1, -1):
         for j in range(6):
-            if belong[j][i] == 0:
-                print(board[j][i], end=' ')
-            if belong[j][i] == 1:
+            if belong[j][i] == None:
+                color_print('w', board[j][i], end=' ')
+            if belong[j][i] == True:
                 color_print('y', board[j][i], end=' ')
-            elif belong[j][i] == 2:
+            elif belong[j][i] == False:
                 color_print('b', board[j][i], end=' ')
         print()
 
@@ -40,22 +40,22 @@ def fall(x):
     n = board[x].count('O')
     for i in range(n):
         board[x].remove('O')
-        belong[x].remove(0)
+        belong[x].remove(None)
     for i in range(n):
         board[x].append('O')
-        belong[x].append(0)
+        belong[x].append(None)
 
 def err():
     print('错误操作。请重试。')
 
 def print_pieces():
     color_print('y', '黄方：')
-    for i in pieces[0]:
+    for i in pieces[1]:
         color_print('y', f'{i}: {pieces[1][i]}')
 
     color_print('b', '蓝方：')
-    for i in pieces[1]:
-        color_print('b', f'{i}: {pieces[1][i]}')
+    for i in pieces[0]:
+        color_print('b', f'{i}: {pieces[0][i]}')
 
 def C(x, y):
     global board, turn
@@ -109,13 +109,13 @@ def put(p_name: str, x, y):
     func(x, y)
 
 turn = True  # T=黄 F=蓝
-rd = 1 # 1黄 2蓝
+rd = 1 # round
 while True:
     print_board()
     print_pieces()
-    print(
+    color_print(
         'y' if turn else 'b',
-        f'第{rd}轮。轮到{"黄" if turn == 1 else "蓝"}方了。'
+        f'第{rd}轮。轮到{"黄" if turn else "蓝"}方了。'
     )
 
     p_name = input('请输入要下的子：').upper()
@@ -123,9 +123,7 @@ while True:
     x -= 1
     y -= 1
 
-    put(p_name)  
+    put(p_name, x, y)  
     
     turn = not turn
     rd += 1
-    
-    
